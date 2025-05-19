e0dd69d84342a51997ef1f0ef17511a8f4a7f740
gabriellefrancine <202301133@iacademy.edu.ph>
2025-05-19 21:27:10 +0800

    log-in ui

--- src/Views/Login.php
+++ src/Views/Login.php
@@ -8,22 +8,41 @@
     <title>Document</title>
 </head>
 <body>
+    <div class="backgImg"> </div>
     <?php include $_SERVER["DOCUMENT_ROOT"] . "/templates/header.php"; ?>
-        <h1>Login Page</h1>
-    <?php if (!empty($error)): ?>
-        <p style="color:red;"><?= htmlspecialchars($error) ?></p>
-    <?php endif; ?>
-    <?php if (!empty($message)): ?>
-        <p style="color:green;"><?= htmlspecialchars($message) ?></p>
-    <?php endif; ?>
-    <form method="post">
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username">
-        <br>
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password">
-        <br>
-        <button type="submit">Login</button>
-    </form>
+
+    <div class="logInContainer">
+        <div class="title">
+            <h1>Login</h1>
+        </div>
+
+        <div class="userCredentials">
+            <?php if (!empty($error)): ?>
+            <p style="color:red;"><?= htmlspecialchars($error) ?></p>
+            <?php endif; ?>
+            <?php if (!empty($message)): ?>
+                <p style="color:green;"><?= htmlspecialchars($message) ?></p>
+            <?php endif; ?>
+            <form method="post">
+                <h2>Student Number</h2>
+                <input type="text" id="username" name="username" placeholder="student number">
+
+                <h2>Password</h2>
+                <input type="password" id="password" name="password" placeholder="password">
+
+                <div class="loginFeatures">
+                    <div class="rememberMe">
+                        <input type="checkbox" id="remember" name="remember" value="Agree">
+                        <label for="terms">Remember Me</label>
+                    </div>
+                    <div class="forgetPass">
+                        <p>Forget Pass?</p>
+                    </div>
+                </div>
+
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    </div>
 </body>
 </html>

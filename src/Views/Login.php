42019640614cd175059b681396aca6bb35662d78
Ellxander <202301138@iacademy.edu.ph>
2025-05-19 15:40:27 +0800

    fixed code structure

--- src/Views/Login.php
+++ src/Views/Login.php
@@ -1,14 +1,14 @@
-<?php include $_SERVER["DOCUMENT_ROOT"] . "/iSIMS-WEB/templates/header.php"; session_start(); ?> 
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/iSIMS-Web/public/assets/CSS/header.css">
     <link rel="stylesheet" href="/iSIMS-Web/public/assets/CSS/login.css">
     <title>Document</title>
 </head>
 <body>
+    <?php include $_SERVER["DOCUMENT_ROOT"] . "/iSIMS-WEB/templates/header.php"; ?> 
         <h1>Login Page</h1>
     <?php if (!empty($error)): ?>
         <p style="color:red;"><?= htmlspecialchars($error) ?></p>
--- templates/header.php
+++ templates/header.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/iSIMS-Web/public/assets/CSS/header.css">
-    <title>Student Access Module</title>
-</head>
-<body>
-    <header class="header_container">
-        <img src="https://i.ibb.co/C5kWMq3R/WHITE-i-ACADEMY-Long-Logo-Makati.png" alt="WHITE-i-ACADEMY-Long-Logo-Makati" border="0">
-    </header>
-</body>
-</html>
+<header class="header_container">
+    <img src="https://i.ibb.co/C5kWMq3R/WHITE-i-ACADEMY-Long-Logo-Makati.png" alt="WHITE-i-ACADEMY-Long-Logo-Makati" border="0">
+</header>

161b6c396fe5c5332d807d11a388b9bba7e615e5
gabriellefrancine <202301133@iacademy.edu.ph>
2025-05-20 10:55:21 +0800

    recoded login

--- src/Views/Login.php
+++ src/Views/Login.php
@@ -5,34 +5,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/public/assets/CSS/header.css">
     <link rel="stylesheet" href="/public/assets/CSS/login.css">
-    <title>Student Access Module</title>
+    <title>Document</title>
 </head>
+    
 <body>
-    <div class="background_image"> </div>
     <?php include $_SERVER["DOCUMENT_ROOT"] . "/templates/header.php"; ?>
+    <div class="backgImg"> </div>
 
-    <main class="main_content">
-        <div class="login_container">
-            <div class="title">
-                <h1>Login</h1>
-            </div>
+    <div class="logInContainer">
+        <div class="title">
+            <h1>Login</h1>
+        </div>
 
+        <div class="userCredentials">
+            <?php if (!empty($error)): ?>
+            <p style="color:red;"><?= htmlspecialchars($error) ?></p>
+            <?php endif; ?>
+            <?php if (!empty($message)): ?>
+                <p style="color:green;"><?= htmlspecialchars($message) ?></p>
+            <?php endif; ?>
             <form method="post" id="login_form">
-                <?php if (!empty($error)): ?>
-                    <p style="color:red;"><?= htmlspecialchars($error) ?></p>
-                <?php endif; ?>
-                
-                <?php if (!empty($message)): ?>
-                    <p style="color:green;"><?= htmlspecialchars($message) ?></p>
-                <?php endif; ?>
+                <div class="student_number">
+                    <label for="Student Number">Student Number</label>
+                    <input type="text" id="username" name="username" placeholder="student number">
+                </div>
 
-                <label for="Student Number">Student Number</label>
-                <input type="text" id="username" name="username" placeholder="student number">
-                    
-                <label for="Password">Password</label>
-                <input type="password" id="password" name="password" placeholder="password">
+                <div class="password">
+                    <label for="Password">Password</label>
+                    <input type="password" id="password" name="password" placeholder="password">
+                </div>
 
-                 <div class="login_features">
+                <div class="login_features">
                     <div class="remember_me">
                         <input type="checkbox" id="remember" name="remember" value="Agree">
                         <label for="terms">Remember Me</label>
@@ -41,10 +44,9 @@
                         <p>Forget Pass?</p>
                     </div>
                 </div>
-                
-                <button type="submit" class="button">Submit</button>
+                <button type="submit">Login</button>
             </form>
         </div>
-    </main>
+    </div>
 </body>
 </html>

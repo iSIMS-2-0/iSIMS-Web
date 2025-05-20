351e414f304bab56d3cb6e843eea8adc7e74633b
Eux <eux@macbookpro>
2025-05-21 06:07:41 +0800

    Fix Code Redundancy, Fix HomePage, Add Color Palette

--- src/Views/HomePage.php
+++ src/Views/HomePage.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="/public/assets/CSS/base.css">
     <link rel="stylesheet" href="/public/assets/CSS/header.css">
     <link rel="stylesheet" href="/public/assets/CSS/homepage.css">
     <title>Document</title>
--- src/Views/Login.php
+++ src/Views/Login.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="/public/assets/CSS/base.css">
     <link rel="stylesheet" href="/public/assets/CSS/header.css">
     <link rel="stylesheet" href="/public/assets/CSS/login.css">
     <title>Document</title>

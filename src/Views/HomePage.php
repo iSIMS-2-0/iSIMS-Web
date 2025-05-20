2b5d82f6e1693356b6e1bcb1d8cc408efed5211a
Eux <eux@macbookpro>
2025-05-21 06:18:15 +0800

    Fix Homepage

--- src/Views/HomePage.php
+++ src/Views/HomePage.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="stylesheet" href="/public/assets/CSS/base.css">
+    <link rel="stylesheet" href="/public/assets/CSS/base.css">
     <link rel="stylesheet" href="/public/assets/CSS/header.css">
     <link rel="stylesheet" href="/public/assets/CSS/homepage.css">
     <title>Document</title>
 </head>
 <body>
-    <?php include $_SERVER["DOCUMENT_ROOT"] . "/templates/header.php"; ?>
+    <?php include $_SERVER["DOCUMENT_ROOT"] . "templates/header.php"; ?>
     <div class="mainContainer">
         <div class="navBar"> 
             <!--Thinking na each section title, may sariling div kase may icon pa, instead na patong patong lang na text href -->
--- src/Views/Login.php
+++ src/Views/Login.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <link rel="stylesheet" href="/public/assets/CSS/base.css">
     <link rel="stylesheet" href="/public/assets/CSS/header.css">
     <link rel="stylesheet" href="/public/assets/CSS/login.css">

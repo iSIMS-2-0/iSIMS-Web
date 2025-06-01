a7dbcb2f10637278bd4f4376b787e741c3648259
Ellxander <202301138@iacademy.edu.ph>
2025-06-01 20:15:13 +0800

    fixed layouts

--- src/Views/HomePage.php
+++ src/Views/HomePage.php
@@ -17,40 +17,44 @@ if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity'] > 
 // Update last activity time
 $_SESSION['last_activity'] = time();*/
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/public/assets/CSS/main.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
+    <link rel="stylesheet" href="/public/assets/CSS/Components/Homepage/homepage.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <script defer src="/public/assets/JavaScript/sidebar.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
     <title>Home</title>
 </head>
 <body>
     <?php include $_SERVER["DOCUMENT_ROOT"] . "/templates/header.php"; ?>
     <?php include $_SERVER["DOCUMENT_ROOT"] . "/templates/sideBar.php"; ?>  
 
-    <div class="mainContainer">  
+    <main class="mainContainer">
         <div class="contents">
             <div class="welcomeMessage">
                 <p>Welcome, <?= htmlspecialchars($_SESSION['user_name']) ?></p>
             </div>
+
             <div class="schoolBulletin">
                 <div class="announcements">
-                    <h2>Announcements</h2>
-                    <p>Lorem ipsum dolor sit amet.</p>
+                    <h1>Announcements</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit, harum. Numquam cupiditate ipsum, quae minima autem amet rem perspiciatis veritatis natus aspernatur doloribus magni. Ex sed dicta laboriosam magni maxime?</p>
                 </div>
+
                 <div class="remarks">
-                    <h2>Remarks</h2>
-                    <p>Lorem ipsum dolor sit amet.</p>
+                    <h1>Remarks</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus officiis quam in nulla debitis aliquid! Veniam cumque rerum quo nobis reprehenderit laudantium explicabo. Dignissimos iure nam fugiat suscipit a exercitationem?</p>
                 </div>
+
                 <div class="calendar">
-                    <h2>Calendar</h2>
+                    <h1>Calendar</h1>
                 </div>
             </div>
         </div>
-    </div>
+    </main>
 </body>
 </html>
--- src/Views/Login.php
+++ src/Views/Login.php
@@ -4,24 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/public/assets/CSS/main.css">
+    <link rel="stylesheet" href="/public/assets/CSS/Components/Login/login.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <script defer src="/public/assets/JavaScript/login.js"></script>
     <title>Login</title>
 </head>
 <body>
     <?php include $_SERVER["DOCUMENT_ROOT"] . "/templates/header.php"; ?>
-    <section class="background_image"></section>
+    <section class="backgroundImage"></section>
 
-    <!-- Loading Overlay -->
-    <div class="loading-overlay" id="loadingOverlay">
-        <div class="loading-spinner"></div>
-    </div>
-
-    <main class="login_container">
-        <header class="title">
+    <main class="loginContainer">
+        <div class="title">
             <h1>Login</h1>
-        </header>
+        </div>
 
-        <section class="user_credentials">
+        <section class="userCredentials">
             <?php if (!empty($error)): ?>
                 <p style="color:red;"><?= htmlspecialchars($error) ?></p>
             <?php endif; ?>
@@ -30,39 +27,36 @@
                 <p style="color:green;"><?= htmlspecialchars($message) ?></p>
             <?php endif; ?>
 
-            <form method="post" id="login_form">
-                <div class="username_field">
+            <form method="post" id="loginForm">
+                <div class="usernameField">
                     <label for="username">Student Number</label>
                     <input type="text" id="username" name="username" placeholder="student number" required>
                 </div>
 
-                <div class="password_field">
+               <div class="password_field">
                     <label for="password">Password</label>
                     <div class="password-container">
                         <input type="password" id="password" name="password" placeholder="password" required>
                         <button type="button" id="togglePassword" class="toggle-password">
-                            <i class="fa-solid fa-eye"></i> <!-- Font Awesome icon -->
-                        </button>
+                            <i class="fa-solid fa-eye"></i> 
+                        </button> 
                     </div>
                 </div>
 
-                <div class="login_features">
-                    <div class="remember_me">
+                <div class="loginOptions">
+                    <div class="rememberMe">
                         <input type="checkbox" id="remember" name="remember" value="Agree">
                         <label for="remember">Remember Me</label>
                     </div>
 
-                    <div class="forget_password">
-                        <a href="#">Forget Password?</a>
+                    <div class="forgotPassword">
+                        <a href="#">Forgot Password?</a>
                     </div>
                 </div>
-                 
+
                 <button type="submit" class="submit">Submit</button>
             </form>
-
         </section>
-
     </main>
-    <script defer src="/public/assets/JavaScript/login.js"></script>
 </body>
 </html>

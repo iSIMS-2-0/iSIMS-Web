e413ba13e2b9304ac16c6f5a1d894403db4acc35
Eux <eux@macbookpro>
2025-06-08 11:19:37 +0800

    resolve conflict

--- public/index.php
+++ public/index.php
@@ -346,26 +346,6 @@ $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 //     $userModel->createCompleteUser($data['student'], $data['family'], $data['medical']);
 // }
 
-<<<<<<< Updated upstream
-=======
-// Simple router
-// $uri = $_SERVER['REQUEST_URI'];
-// echo "<h2>Debug: URI = " . htmlspecialchars($uri) . "</h2>";
-// $user = new User($pdo);
-// $user->createUser(
-//     '202301042',
-//     'John Doe',
-//     'BS Computer Science',
-//     'Male',
-//     'prefer not to say',
-//     '09123456789',
-//     '0123456789',
-//     'johndoeuwu123@gmail.com',
-//     '123 Main St, City, Country',
-//     'johndoe123' // Password should be hashed in the model
-// );
-
->>>>>>> Stashed changes
 $controller = new AuthController($pdo);
 $controller->login();
 ?>

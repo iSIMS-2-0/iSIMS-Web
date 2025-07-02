db32e77ae17b05ff565b05678957be06597828da
gabriellefrancine <202301133@iacademy.edu.ph>
2025-07-02 21:28:44 +0800

    erf adjustments + all.css fix

--- src/Views/Payment/erf.php
+++ src/Views/Payment/erf.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/public/assets/CSS/main.css">
-    <link rel="stylesheet" href="/public/assets/CSS/Components/Registration/erf.css">
+    <link rel="stylesheet" href="/public/assets/CSS/Components/Payment/erf.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <script defer src="/public/assets/JavaScript/sidebar.js"></script>
     <title>ERF</title>
@@ -16,6 +16,47 @@
     <div class="mainContainer">
         <div class="contents">
             <h1>Electronic Registration Form
+            <form method="get">
+                <div class="selection">
+                    <div class="syDiv">
+                        <label for="schoolYear">School Year:</label>  
+                        <select name="schoolYear" id="schoolYear" onchange="this.form.submit()">
+                            <option value="<?= htmlspecialchars($selected_sy) ?>"><?= htmlspecialchars($selected_sy) ?></option>
+                        </select>
+                    </div>
+                
+                    <div class="termDiv">
+                        <label for="term">Term:</label>
+                        <select name="term" id="term" onchange="this.form.submit()">
+                            <option value="1st Term"<?= $selected_term=='1st Term'?' selected':''; ?>>1st Term</option>
+                            <option value="2nd Term"<?= $selected_term=='2nd Term'?' selected':''; ?>>2nd Term</option>
+                            <option value="3rd Term"<?= $selected_term=='3rd Term'?' selected':''; ?>>3rd Term</option>
+                        </select>
+                    </div>
+                </div>
+            </form>
+
+            <div class="erfAndTable">
+                <div class="erfFrame">
+                    .
+                </div>
+                <div class="paymentMethods">
+                    <div class="paymentTitle">
+                        <h5>Select Payment Methods</h5>
+                    </div>
+                    <div class="paymentButtons">
+                            <a href="https://tinyurl.com/Card-Online-Payment" class="cardPayment">
+                                <i class="fa-solid fa-building-columns"></i></i> Online Bank Transfer
+                            </a>
+
+                            <a href="https://tinyurl.com/Card-Online-Payment" class="cardPayment">
+                                <i class="fas fa-credit-card"></i> Credit Card Payment
+                            </a>
+
+                            <p>Note: upload proof of payment on Online Payment Page</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </body>

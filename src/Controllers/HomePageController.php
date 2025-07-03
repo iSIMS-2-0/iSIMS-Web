3e31069ca7db28474dea537092292284dfbac519
Eux <euclidguillermo28@gmail.com>
2025-07-04 04:16:52 +0800

    Improve Code Readabiliy HomePageController

--- src/Controllers/HomePageController.php
+++ src/Controllers/HomePageController.php
@@ -19,6 +19,24 @@ class HomePageController {
         // idk maybe process some shit here
         // but for now
         // Render the home page view
+        
+        session_start();
+        // Check if the user is logged in
+        if (!isset($_SESSION['user_id'])) {
+            header("Location: /public/index.php?message=Please log in to access this page.");
+            exit();
+        }
+
+        // Check for session timeout
+        if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity'] > 600)) { // 600 seconds = 10 minutes
+            // Session has expired
+            session_unset();
+            session_destroy();
+            header("Location: /public/index.php?message=Session expired. Please log in again.");
+            exit();
+        }
+        // Update last activity time
+        $_SESSION['last_activity'] = time();
        require __DIR__ . '/../Views/HomePage.php';
     }
 }
